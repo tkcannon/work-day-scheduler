52db4d93c18a4432df94a70c47fe0f44d02586a0
tkcannon <taylorkcannon@gmail.com>
2021-11-26 16:31:03 -0700

    added save function

--- assets/script.js
+++ assets/script.js
@@ -26,19 +26,36 @@ function loadSchedule() {
         var hour = $("<div>").addClass("hour col-1");
         var hourtext = $("<p>").addClass("p-3").text(schedule.slot[i][0]);
         hour.append(hourtext);
-        // items to do
-        var toDoList = $("<textarea>").addClass("past col-10");
-        var toDo = $("<p>").text(schedule.slot[i][1]);
-        toDoList.append(toDo);
+        // text content of time-block
+        var text = $("<textarea>").addClass("past col-10 text-area").text(schedule.slot[i][1]);
         // save button
-        var saveButton = $("<button>").addClass("saveBtn col-1");
+        var saveButton = $("<button>").addClass("saveBtn col-1").attr("id", schedule.slot[i][0]); // adds id of the hour so button clicks can check against array data in schedule
         var saveIcon = $("<i>").addClass("fas fa-save");
         saveButton.append(saveIcon);
-        timeBlock.append(hour, toDoList, saveButton,);
+        // add all to time-block
+        timeBlock.append(hour, text, saveButton,);
 
-        //add all to container
+        // add time-block to container
         $(".container").append(timeBlock);
     }
 }
+
+function saveSchedule() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+// when save button clicked
+$(".container").on("click", "button", function () {
+    var id = $(this).attr("id");
+    // iterates through schedule until slot with time matching save button id is found
+    for (let i = 0; i < schedule.slot.length; i++) {
+        if (id === schedule.slot[i][0]) {
+            //changes text of data to save to match text in time-block
+            text = $(this).siblings(".text-area").val().trim();
+            schedule.slot[i][1] = text;
+            saveSchedule();
+        }
+    }
+});
 
 loadSchedule();

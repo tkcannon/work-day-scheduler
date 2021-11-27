123e2eb1082d9f483d00a7baa93664823cd61ce9
tkcannon <taylorkcannon@gmail.com>
2021-11-27 10:28:27 -0700

    final adjustments

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 var schedule = {};
-//tim vars
-var now = dayjs();
+//time vars
+var now = dayjs(); // timestamp at load
 $("#currentDay").text(now.format("dddd, MMMM D"));
-var firstRefresh = now.add(1, 'h').subtract(now.minute(), "m").subtract(now.second(), "s").subtract(now.millisecond(), "ms");
-var delay = firstRefresh.diff(now);
-console.log(delay);
+var firstReload = now.add(1, 'h').subtract(now.minute(), "m").subtract(now.second(), "s").subtract(now.millisecond(), "ms"); // the next even hour from page load
+var delay = firstReload.diff(now); // leftover time in this hour
 
 function loadSchedule() {
+    $(".container").empty();
     schedule = JSON.parse(localStorage.getItem("schedule"));
 
     // if no existing schedule exists, create new empty schedule obj
@@ -27,15 +27,15 @@ function loadSchedule() {
     }
 
     for (i = 0; i < schedule.slot.length; i++) {
-        // time
+        // time block info
         var timeBlock = $("<div>").addClass("time-block row");
-        var hour = $("<div>").addClass("hour col-1");
+        var hour = $("<div>").addClass("col-sm-2 col-md-1 hour");
         var hourtext = $("<p>").addClass("p-3").text(schedule.slot[i][0]);
         hour.append(hourtext);
         // text content of time-block
-        var text = $("<textarea>").addClass("col-10 text-area " + timeState(i)).text(schedule.slot[i][1]);
+        var text = $("<textarea>").addClass("col-sm-8 col-md-10 text-area " + timeState(i)).text(schedule.slot[i][1]);
         // save button
-        var saveButton = $("<button>").addClass("saveBtn col-1").attr("id", schedule.slot[i][0]); // adds id of the hour so button clicks can check against array data in schedule
+        var saveButton = $("<button>").addClass("col-sm-2 col-md-1 saveBtn").attr("id", schedule.slot[i][0]); // adds id of the hour so button clicks can check against array data in schedule
         var saveIcon = $("<i>").addClass("fas fa-save");
         saveButton.append(saveIcon);
         // add all to time-block
@@ -50,8 +50,9 @@ function saveSchedule() {
     localStorage.setItem("schedule", JSON.stringify(schedule));
 }
 
+// checks schedule times based on index and returns past, present, or future
 function timeState(slotIndex) {
-    var startTime = 9
+    var startTime = 9;
     if (now.hour() < slotIndex + startTime) {
         return ("future");
     }
@@ -78,8 +79,9 @@ $(".container").on("click", "button", function () {
 });
 loadSchedule();
 
+// first reload counts down from leftover time in hour so all subsequent reloads are every hour on the hour
 setInterval(function () {
+    now = now.add(1, "h");
     loadSchedule();
     delay = 360000;
-    console.log("Reloaded Schedule at: ", dayjs());
 }, delay);
